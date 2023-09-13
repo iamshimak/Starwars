77dac7585636e1179f3d9bc79dadaf48d5a1fde2
Ahamed Shimak <shimak.a@aeturnum.com>
2023-09-13 12:22:52 +0530

    Initial Commit

---
+++ StarWars/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StarWars
+//
+//  Created by Ahamed Shimak on 2023-09-13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
